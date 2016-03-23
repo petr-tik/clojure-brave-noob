13c6538c769dd20aa5d0dae53f96128bf1309917
petr-tik <tikilyaynen@gmail.com>
2016-03-23 22:57:08 +0000

    implemented the error message function

--- src/clojure_brave_noob/core.clj
+++ src/clojure_brave_noob/core.clj
@@ -1,7 +1,22 @@
 (ns clojure-brave-noob.core
   (:gen-class))
 
+(defn solveMeFirst [x y]
+    (println (+ x y)))
+
+(defn error-message 
+    [severity]
+    (str "OH MY GOD! "
+        (if (= severity :mild)
+        "This is a minor inconvinience"
+        "This shit is getting serious")))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "I am a little teapot"))
+  (println "I am a little teapot")
+  (solveMeFirst 5 10)
+  (println (error-message :mild))
+  (error-message :hard)
+)
+
