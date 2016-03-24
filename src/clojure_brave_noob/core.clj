38b24a83c96b6d6e8bce4e25615df2a795f82b0c
petr-tik <tikilyaynen@gmail.com>
2016-03-24 22:10:59 +0000

    working through core functions chapter

--- src/clojure_brave_noob/core.clj
+++ src/clojure_brave_noob/core.clj
@@ -11,6 +11,36 @@
         "This is a minor inconvinience"
         "This shit is getting serious")))
 
+(defn element-print 
+    [times elements]
+    (println))
+
+(def vampire-database
+{
+    0 {:makes-blood-puns? false, :has-pulse? true :name "lad"}
+    1 {:makes-blood-puns? false, :has-pulse? true :name "bob"}
+    2 {:makes-blood-puns? true, :has-pulse? false :name "john"}
+    3 {:makes-blood-puns? true, :has-pulse? true :name "Petr"}})
+
+(defn vampire-related-details
+    [social-security-number]
+    (Thread/sleep 1000)
+    (get vampire-database social-security-number))
+
+(defn vampire?
+    [suspect]
+    (and (:makes-blood-puns? suspect)
+            (not (:has-pulse? suspect))
+        suspect))
+
+(defn identify-vampire
+    [social-security-numbers]
+    (first (filter vampire?
+        (map vampire-related-details social-security-numbers)))
+
+    )
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
@@ -18,5 +48,4 @@
   (solveMeFirst 5 10)
   (println (error-message :mild))
   (error-message :hard)
-)
-
+  (time (vampire-related-details 0)))
