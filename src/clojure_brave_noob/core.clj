a00a5aa3a2f02cc36ce8400dd6b621840412e3bd
petr-tik <tikilyaynen@gmail.com>
2016-03-27 17:00:04 +0100

    finished chapter 4

--- src/clojure_brave_noob/core.clj
+++ src/clojure_brave_noob/core.clj
@@ -53,6 +53,8 @@
     )
 
 
+(def filename "suspects.csv")
+
 ;; vector of keys needed to create a vampire own map
 (def vamp-keys [:name :glitter-index])
 
@@ -70,8 +72,30 @@
   [vamp-key value]
   ((get conversions vamp-key) value))
 
+(defn parse
+  "Convert a CSV into rows and columns"
+  [string] ;; csv slurped as string
+  (map #(clojure.string/split % #",")
+    (clojure.string/split string #"\n")))
+
+(defn mapify 
+  "Return a seq of maps in form
+  {:name \"Edward Cullen\" :glitter-index 10}"
+  [rows]
+  (map (fn [unmapped-row]
+      (reduce (fn [row-map [vamp-key value]]
+        (assoc row-map vamp-key (convert vamp-key value)))
+      {}
+      (map vector vamp-keys unmapped-row)))
+  rows))
+
+(defn glitter-filter
+  [minimum-glitter records]
+  (filter #(>= (:glitter-index %) minimum-glitter) records))
+
 
 (defn -main
   [& args]
   println "hello"
+
   )
